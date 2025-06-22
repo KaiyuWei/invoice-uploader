759f6a9f7cee0c8c33fe2b99a9d175559edc9224
KaiyuWei <weikaiyu2020@outlook.com>
2025-06-22 22:13:11 +0200

    debug docker-compose

--- app/Http/Controllers/Api/SalesInvoiceController.php
+++ app/Http/Controllers/Api/SalesInvoiceController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UploadInvoiceRequest;
 use App\Models\SalesInvoice;
-use App\Models\InvoiceLine;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class SalesInvoiceController extends Controller
